b1f1ca2cd8eccfde6ac10cc85cf2a755069dede2
Seoyoon Park <Seoyoon.Park@viacom.com>
2018-09-11 16:27:49 -0400

    Initialize project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject signafire "0.1.0-SNAPSHOT"
+  :description "Signafire take home test answers"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
